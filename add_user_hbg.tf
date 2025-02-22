37b5f59cf152fbdbe30202edc08981200a75034e
Pablo Menezes <pmenezes.aju@gmail.com>
2025-02-21 22:00:31 -0300

    Remove setup rds

--- add_user_hbg.tf
+++ add_user_hbg.tf
@@ -1,21 +1,21 @@
-resource "null_resource" "db_setup" {
-  depends_on = [ aws_db_instance.postgresql ]
-  triggers = {
-    db_instance = aws_db_instance.postgresql.id
-
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-psql --host=${aws_db_instance.postgresql.address} \
-     --port=5432 \
-     --username=${var.master_username} \
-     --dbname=${var.database_name} \
-     --command="CREATE USER sailer WITH PASSWORD '${random_password.master_password.result}'; GRANT CONNECT ON DATABASE ${var.database_name} TO sailer;"
-EOT
-    environment = {
-      PGPASSWORD = random_password.master_password.result
-    }
-  }
-}
+#resource "null_resource" "db_setup" {
+#  depends_on = [ aws_db_instance.postgresql ]
+#  triggers = {
+#    db_instance = aws_db_instance.postgresql.id
+#
+#  }
+#
+#  provisioner "local-exec" {
+#    command = <<EOT
+#psql --host=${aws_db_instance.postgresql.address} \
+#     --port=5432 \
+#     --username=${var.master_username} \
+#     --dbname=${var.database_name} \
+#     --command="CREATE USER sailer WITH PASSWORD '${random_password.master_password.result}'; GRANT CONNECT ON DATABASE ${var.database_name} TO sailer;"
+#EOT
+#    environment = {
+#      PGPASSWORD = random_password.master_password.result
+#    }
+#  }
+#}
 
