f86df4ef8640288261abff84512e32cfa2407620
Pablo Menezes <pmenezes.aju@gmail.com>
2025-03-03 15:50:53 -0300

    Remove provider file in module

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
+#terraform {
+#  required_providers {
+#    random = {
+#      source  = "hashicorp/random"
+#      version = "3.6.3"
+#    }
+#  }
+#}
+#
+#provider "aws" {
+#  region = var.region
+#}
 
